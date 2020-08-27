c317ce59172965ed707b48882a8bfc3156ef702c
Evgeniy Orekhov <thebettermay@gmail.com>
2020-08-27 18:02:12 +0300

    svg change commit

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
       {
         test: /\.(png|jpg|gif|ico|svg)$/,
         use: [
-          "file-loader?name=./src/images/[name].[ext]",
+          "file-loader?name=./images/[name].[ext]",
           {
             loader: "image-webpack-loader",
             options: {},
