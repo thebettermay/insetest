2696b91a37818959b92b0e733d7b7d870e205427
Evgeniy Orekhov <thebettermay@gmail.com>
2020-08-27 17:23:07 +0300

    svg change commit

--- src/js/components/Email.js
+++ src/js/components/Email.js
@@ -5,7 +5,7 @@ export default class Email {
   content(email) {
     console.log(this.func);
     const template = `<th onclick="location.href='mailto:${email}';" class="email">
-          <img class="image" src='./src/images/envelope-solid.svg' </th>`;
+          <img class="image" src="<%=require('./images/envelope-solid.svg')%>" </th>`;
     return template;
   }
 }
